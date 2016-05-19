2a2b88a526f22dfad1c51d913c69137c501fec77
CleverboyDev <games.juliso@gmail.com>
2016-05-19 18:37:45 +0200

    First content

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,15 @@
+package com.example.examplemod;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "theutilitymod", version = "1.0", name = "The Utility Mod")
+public class ExampleMod {
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+}
